0377b719c66075a4baeb82bbba9ae593b93d3211
damienteo <damien.teo.ks@gmail.com>
2022-10-15 00:27:14 +0800

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
+const { STAGING_QUICKNODE_KEY, PRIVATE_KEY, ETHERSCAN_API } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: STAGING_QUICKNODE_KEY,
+      accounts: [PRIVATE_KEY || ""],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
+  },
+  paths: { tests: "tests" },
 };
 
 export default config;
