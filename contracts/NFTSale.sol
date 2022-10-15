bfe7025e5dbdd507b5dfbf1c13f7698a988ee8ae
damienteo <damien.teo.ks@gmail.com>
2022-10-15 11:40:05 +0800

    Add NFTSale contract

---
+++ contracts/NFTSale.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ThunderDomeNFT.sol";
+
+contract NFTSale is Ownable, ReentrancyGuard {
+    uint256 public nftPrice;
+    ThunderDomeNFT public NFTContract;
+    uint256 public adminPool;
+
+    event Purchased(address buyer, uint256 tokenId, uint256 amountReceived);
+    event Withdrawn(address withdrawer, uint256 amount);
+
+    constructor(uint256 _nftPrice, address _NFTContract) payable {
+        nftPrice = _nftPrice;
+        NFTContract = ThunderDomeNFT(_NFTContract);
+    }
+
+    function purchaseNFT(uint256 tokenId) public payable nonReentrant {
+        uint256 etherReceived = msg.value;
+
+        require(etherReceived > nftPrice);
+
+        adminPool += etherReceived;
+
+        NFTContract.transferFrom(address(this), msg.sender, tokenId);
+
+        emit Purchased(msg.sender, tokenId, etherReceived);
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        require(amount <= adminPool, "Amount less than available");
+        adminPool -= amount;
+
+        (bool success, ) = (msg.sender).call{value: amount}("");
+        require(success, "Failed to withdraw money from contract");
+
+        emit Withdrawn(msg.sender, amount);
+    }
+}
