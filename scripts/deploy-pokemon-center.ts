855d83151e73b4a0df12dd4c7ea26401304cab66
damienteo <damien.teo.ks@gmail.com>
2022-10-21 20:06:50 +0800

    Add script for deploying pokemon center

---
+++ scripts/deploy-pokemon-center.ts
@@ -0,0 +1,26 @@
+import { ethers } from "hardhat";
+
+const { THUNDERDOME_NFT_ADDRESS = "" } = process.env;
+
+async function deployPokemonCenter() {
+  const PokePoint = await ethers.getContractFactory("PokePoint");
+  const PokemonCenter = await ethers.getContractFactory("PokemonCenter");
+
+  const pokePoint = await PokePoint.deploy();
+
+  console.log(`PokePoint Contract deployed to ${pokePoint.address}`);
+
+  const pokemonCenter = await PokemonCenter.deploy(
+    pokePoint.address,
+    THUNDERDOME_NFT_ADDRESS
+  );
+
+  console.log(`Pokemon Center Contract deployed to ${pokemonCenter.address}`);
+}
+
+deployPokemonCenter()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
